69748925ce4730a647a194277be68ff33be8ea8f
himakshivaishnav122-bit <himakshivaishnav122@gmail.com>
2025-10-29 19:26:44 +0530

    Update vars.py

--- Modules/vars.py
+++ Modules/vars.py
@@ -3,19 +3,20 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23712517"))
-API_HASH = environ.get("API_HASH", "334d992b622e393e838a7c21a1aded67")
+API_ID = int(environ.get("API_ID", "22187182"))
+API_HASH = environ.get("API_HASH", "28a7bb842e7bf1f4219248b5a5a51a8b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "8312514574"))
+OWNER = int(environ.get("OWNER", "5148965221"))
 CREDIT = environ.get("CREDIT", "RISHI")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8312514574').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5148965221').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8312514574').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5148965221').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
